2f46d9ab36815555d38aca32845c47485ec03b63
laboratorio <laboratorio@UPOAULA10306>
2025-01-21 07:14:47 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP3StefanJativa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenP3StefanJativa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenP3StefanJativa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenP3StefanJativa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
