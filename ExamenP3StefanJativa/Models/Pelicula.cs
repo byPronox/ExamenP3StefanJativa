4a5c2a96c4e7b978dc97f437198e6544f495027e
byPronox <bypronoxyt@gmail.com>
2025-01-21 07:34:35 -0500

    Commit 2 Creacion del Model

---
+++ ExamenP3StefanJativa/Models/Pelicula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ExamenP3StefanJativa.Models
+{
+    internal class Pelicula
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string TituloPelicula { get; set; }
+        public string Genero { get; set; }
+        public string ActorPrincal { get; set; }
+        public string Awards { get; set; }
+        public string Webside { get; set; }
+        public string Usuario { get; set; } = "SJativa";
+        public string Descripcion { get; set; }
+    }
+}
---
+++ ExamenP3StefanJativa/Views/LecturaPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenP3StefanJativa.Views;
+
+public partial class LecturaPage : ContentPage
+{
+	public LecturaPage()
+	{
+		InitializeComponent();
+	}
+}
